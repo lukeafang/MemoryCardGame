eafa5919b227f4df08ee801753fb0ef788f3f062
Kuochi.Fang <lukeafang@gmail.com>
2025-02-23 10:04:16 -0600

    Initial Commit

---
+++ MemoryCardGame/MemoryCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryCardGameApp.swift
+//  MemoryCardGame
+//
+//  Created by Kuochi Fang on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
